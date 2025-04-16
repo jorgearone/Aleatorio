1cfa1e3893f70ec34acb411964ab683e71d33fc0
jorgearone <48113927+jorgearone@users.noreply.github.com>
2025-04-16 11:15:09 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  /* cualquier otra configuración que ya exista */
 };
 
 export default nextConfig;
